56deea7b6abb8a5cc2d4846b4c1ee6d4bc9ef992
SangJin <70688424+Hansangjin98@users.noreply.github.com>
2021-11-23 02:22:22 +0900

    [FEAT] 프로젝트 초기 파일 생성

---
+++ SOT-iOS/SOT-iOS/Sources/HomeViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  SOT-iOS
+//
+//  Created by 한상진 on 2021/11/23.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+
+}
+
